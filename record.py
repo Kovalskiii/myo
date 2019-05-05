1f306ae45ed0a0c947768e21a84049e150cc973a
Vlad Kovalski <vladkoval_97@list.ru>
2019-05-05 20:03:47 +0300

    owo

--- record.py
+++ record.py
@@ -9,12 +9,12 @@ emg_data = []
 myo.add_emg_handler(lambda emg, _: emg_data.append(emg))
 
 def record(gesture_name, time2record=2.5):
-    out = open(DATA_PATH + gesture_name, 'a')
     start = time.time()
     while time.time() - start <= time2record:
         myo.run()
-    out.write("\n")
-    out.write(str(emg_data))
+    with open(DATA_PATH + gesture_name, 'a') as out:
+        out.write("\n")
+        out.write(str(emg_data))
 
 myo.add_emg_handler(record)
 

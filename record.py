5a5264699ace086928c52df0d2c24f2efc11c56f
Vlad Kovalski <vladkoval_97@list.ru>
2019-05-05 20:30:02 +0300

    owo

--- record.py
+++ record.py
@@ -1,19 +1,20 @@
 import sys
 import time
 from pyoconnect.myo_raw import MyoRaw
-DATA_PATH = 'data/'
+DATA_PATH = 'data'
 
 myo = MyoRaw()
 emg_data = []
 myo.add_emg_handler(lambda emg, _: emg_data.append(emg))
 
-def record(gesture_name, time2record=2.5):
+def record(gesture_name, write_to=DATA_PATH, time2record=2.5):
     myo.connect()
     start = time.time()
     while time.time() - start <= time2record:
         myo.run()
-    with open(DATA_PATH + gesture_name, 'a+') as out:
+    with open(write_to, 'a') as out:
         out.write("\n")
+        out.write(gesture_name)
         out.write(str(emg_data))
     emg_data.clear()
 

28adba2786f37053b34f9f662a6fa724d06a1584
Vlad Kovalski <vladkoval_97@list.ru>
2019-05-05 18:58:40 +0300

    owo

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from pyoconnect.myo_raw import MyoRaw
-from servo import gesture_callback
+# from servo import gesture_callback
 m = MyoRaw()
 
 m.add_pose_handler(print)
-m.add_pose_handler(gesture_callback)
+# m.add_pose_handler(gesture_callback)
 # m.add_emg_handler(print)
 m.connect()
 

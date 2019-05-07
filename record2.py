f2832ed3e0e558c24e22f5a0a568e5ccaaa1a441
Vlad Kovalski <vladkoval_97@list.ru>
2019-05-07 19:47:19 +0300

    record2

---
+++ record2.py
@@ -0,0 +1,42 @@
+import sys
+import time
+from pyoconnect.myo_raw import MyoRaw
+import threading
+from stuff import Gesture
+import itertools
+
+
+myo = MyoRaw()
+emg_data = []
+recording = False
+@myo.add_emg_handler
+def record(*args):
+    if recording:
+        emg_data.append(args[0])
+
+def myo_run():
+    myo.connect()
+    while True:
+        myo.run()
+
+
+if __name__ == '__main__':
+    myo_thread = threading.Thread(target=myo_run, daemon=True)
+    myo_thread.start()
+    write_to = input("File to write: ")
+    time2record = float(input("Time to record: "))
+    
+    for gesture in stuff.Gesture:
+        gesture_name = gesture.name
+        input("Show {} and press enter".format(gesture_name))
+        print("Recording...")
+        recording = True
+        time.sleep(time2record)
+        recording = False
+        with open(write_to, 'a') as out:
+            out.write(gesture_name)
+            out.write(' ')
+            out.write(str(emg_data))
+            out.write('\n')
+            print("Flushed {} of samples for {}".format(len(emg_data), gesture_name))
+        emg_data.clear()
--- stuff.py
+++ stuff.py
@@ -4,6 +4,16 @@ class Gesture(Enum):
     Fist = 0
     Fuck = 1
     Like = 2
+    Rest = 3
+    Peace = 4
+    Pinky = 5
+    Ring = 6
+    Middle = 7
+    Index = 8
+    Thumb = 9
+    Spider = 10
+    Rock = 11
+    Rasta = 12
 
 
 class Config:

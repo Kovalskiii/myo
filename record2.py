f8d907f6ae3d26937c867e24b13f3a6ba23bbad1
Vlad Kovalski <vladkoval_97@list.ru>
2019-05-07 19:59:53 +0300

    yesman

--- record2.py
+++ record2.py
@@ -31,17 +31,19 @@ if __name__ == '__main__':
     write_to = input("File to write: ")
     time2record = float(input("Time to record: "))
     
-    for gesture in Gesture:
-        gesture_name = gesture.name
-        input("Show {} and press enter".format(gesture_name))
-        print("Recording...")
-        recording = True
-        time.sleep(time2record)
-        recording = False
-        with open(write_to, 'a') as out:
-            out.write(gesture_name)
-            out.write(' ')
-            out.write(str(emg_data))
-            out.write('\n')
-            print("Flushed {} of samples for {}".format(len(emg_data), gesture_name))
-        emg_data.clear()
+    while True:
+        for gesture in Gesture:
+            gesture_name = gesture.name
+            input("Show {} and press enter".format(gesture_name))
+            print("Recording...")
+            recording = True
+            time.sleep(time2record)
+            recording = False
+            with open(write_to, 'a') as out:
+                out.write(gesture_name)
+                out.write(' ')
+                out.write(str(emg_data))
+                out.write('\n')
+                print("Flushed {} of samples for {}".format(len(emg_data), gesture_name))
+            emg_data.clear()
+        print("End of the gesture loop")

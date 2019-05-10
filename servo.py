32e18c092ba350e82717d29544b7a8e24f385769
Vlad Kovalski <vladkoval_97@list.ru>
2019-05-10 20:36:29 +0300

    owo

--- servo.py
+++ servo.py
@@ -1,24 +1,27 @@
 
-from board import SCL, SDA
-import busio
+# from board import SCL, SDA
+# import busio
 
 # Import the PCA9685 module.
-from adafruit_pca9685 import PCA9685
-
+# from adafruit_pca9685 import PCA9685
+from adafruit_servokit import ServoKit
 # This example also relies on the Adafruit motor library available here:
 # https://github.com/adafruit/Adafruit_CircuitPython_Motor
-from adafruit_motor import servo
+# from adafruit_motor import servo
 # from pyoconnect.myo_raw import Pose
 from stuff import Gesture
 
-i2c = busio.I2C(SCL, SDA)
+# i2c = busio.I2C(SCL, SDA)
 
 # Create a simple PCA9685 class instance.
-pca = PCA9685(i2c)
-pca.frequency = 60
+# pca = PCA9685(i2c)
+# pca.frequency = 60
 
-servos = [servo.Servo(pca.channels[i], min_pulse=125, max_pulse=575) 
-          for i in range(1, 6)]
+# servos = [servo.Servo(pca.channels[i], min_pulse=125, max_pulse=575) 
+#           for i in range(1, 6)]
+
+kit = ServoKit(channels=8)
+servos = [kit.servo[i] for i in range(1, 6)]
 
 gestures = {
     Gesture.Rest: (20, 50, 30, 30, 30, 0),

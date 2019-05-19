2d362e49cf44c61c648423d1bf3beac43c6f470d
Vlad Kovalski <vladkoval_97@list.ru>
2019-05-19 17:20:31 +0300

    yas

---
+++ yas.py
@@ -0,0 +1,21 @@
+import RPi.GPIO as GPIO
+from time import sleep
+GPIO.setmode(GPIO.BOARD)
+
+GPIO.setup(29, GPIO.OUT)
+pwm=GPIO.PWM(29, 50)
+pwm.start(0)
+
+
+def SetAngle(angle):
+	duty = angle / 18 + 2
+	GPIO.output(29, True)
+	pwm.ChangeDutyCycle(duty)
+	sleep(1)
+	GPIO.output(29, False)
+	pwm.ChangeDutyCycle(0)
+
+SetAngle(90)
+
+pwm.stop()
+GPIO.cleanup()

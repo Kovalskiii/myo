8467c7d96a4c7a227606256bb1303d957c9157ad
rasppi <fake_pi@pi.com>
2019-05-22 19:52:51 +0300

    ffff

--- gesture.py
+++ gesture.py
@@ -8,7 +8,7 @@ import time
 class Gesturee:
     def __init__(self, model, sample_size=Config.SAMPLE_SIZE, emg_max=Config.EMG_MAX, gesture_delay=Config.GESTURE_DELAY):
         self.model = model
-        self.buff = deque()
+        self.buff = deque(maxlen=sample_size)
         self.sample_size = sample_size
         self.emg_max = emg_max
         self.gesture_delay = gesture_delay
@@ -24,12 +24,11 @@ class Gesturee:
 
     def emg_handle(self, emg, dir=None):
         self.buff.append(emg)
-        if len(self.buff) > self.SAMPLE_SIZE:
-            self.buff.popleft()
-    
+
     def detect_gesture(self):
         if len(self.buff) < self.sample_size:
             return
+        print(self.buff)
         _input = np.array([self.buff]) / self.emg_max
         predictions = self.model.predict(_input)
         y = Gesture(predictions.argmax())
